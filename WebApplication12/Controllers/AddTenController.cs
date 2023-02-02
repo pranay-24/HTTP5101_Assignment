f09d8a42134cdaf5856cd2b8519d58eea9eaf86a
Pranay Mangal <mangal.pranay@gmail.com>
2023-02-02 00:12:10 -0500

    modifications complete

--- WebApplication12/Controllers/AddTenController.cs
+++ WebApplication12/Controllers/AddTenController.cs
@@ -8,12 +8,16 @@ using System.Web.Http;
 namespace WebApplication12.Controllers
 {
     public class AddTenController : ApiController
-    {   
+    {
         /// <summary>
-        /// 
+        ///  API to return an input with 10 added to it 
         /// </summary>
         /// <param name="num">Integer input</param>
-        /// <returns></returns>
+        /// <returns> Result of addition </returns>
+        ///  <example>
+        /// GET http://localhost:59124/api/AddTen/10  -> 20 
+        /// </example>
+        /// 
         [HttpGet]
         [Route("api/AddTen/{num}")]
         public int Get( int num)
